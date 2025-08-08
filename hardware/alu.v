8421760cb9f800303197b3e7a7429a2ccce1cbaa
VijayU2205 <vijayu.ec24@rvce.edu.in>
2025-08-08 15:50:13 +0530

    Update alu.v

--- hardware/alu.v
+++ hardware/alu.v
@@ -1,13 +1,14 @@
-module alu( 
-input[31:0] a, b,
-  input  [2:0]  op,
-  output [31:0] result
+module alu (
+    input [31:0] a, b,
+    input [2:0] op,
+    output reg [31:0] result
 );
-  always @(*) begin
-    case (op)
-      3'b000: result = a + b;   // ADD
-      3'b001: result = a - b;   // SUB
-      default: result = 32'b0;
-    endcase
-  end
+    always @(*) begin
+        case(op)
+            3'b000: result = a + b;    // ADD
+            3'b001: result = a - b;    // SUB
+            3'b010: result = a & b;    // AND
+            default: result = 0;
+        endcase
+    end
 endmodule
